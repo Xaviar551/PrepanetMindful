0512357609b44601ff5dcb8401363aa85e443941
Javier Prieto <a01610324@itesm.mx>
2021-10-21 11:56:09 -0500

    First commit

---
+++ PrepanetMindful/PrepanetMindful/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrepanetMindful
+//
+//  Created by user207679 on 10/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
