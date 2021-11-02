2f3ca615e4eb47d267367601f018069177491643
Cristian Omar Urbina Herrera <cristianomarurbinaherrera@MacBook-Air-de-Cristian.local>
2021-11-02 11:44:45 -0700

    Add login view mockup

--- PrepanetMindful/PrepanetMindful/ViewController.swift
+++ PrepanetMindful/PrepanetMindful/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var tfId: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    func prepareView() {
+        loginButton.layer.cornerRadius = 15.0
+        loginButton.layer.borderWidth = 2.0
+        loginButton.layer.borderColor = UIColor.systemGray.cgColor
+        tfId.layer.cornerRadius = 15.0
+        tfId.layer.borderWidth = 2.0
+        tfId.layer.borderColor = UIColor.systemGray.cgColor
+        tfPassword.layer.cornerRadius = 15.0
+        tfPassword.layer.borderWidth = 2.0
+        tfPassword.layer.borderColor = UIColor.systemGray.cgColor
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        prepareView()
+        
     }
 
 
