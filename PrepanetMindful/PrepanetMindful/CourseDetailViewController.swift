8dcbb1c6eaa456fa70a391b4b3a2fe9ad64aa0c7
Ricardo Rodríguez <ricardo.rodriguez.rdz9@gmail.com>
2021-11-04 00:02:33 -0500

    Creada vista de cursos y detalle de curso

---
+++ PrepanetMindful/PrepanetMindful/CourseDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CourseDetailViewController.swift
+//  PrepanetMindful
+//
+//  Created by user202610 on 11/3/21.
+//
+
+import UIKit
+
+class CourseDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
